777a754feff4eb5e1cc9d68e4ecd87daca3cca9b
CaioAlexanderx <caio.pantoja@gmail.com>
2026-04-23 10:59:12 -0300

    feat(private): mount productsVariations router ANTES de products
    Endpoints expostos:
    - GET  /companies/:id/products/:pid/variations
    - PUT  /companies/:id/products/:pid/variations
    
    Registrado ANTES de ./products pra ordem Express correta.

--- src/routes/private.js
+++ src/routes/private.js
@@ -26,6 +26,7 @@ router.use('/pdv', require('./pdv'));
 router.use('/', require('./pdvSettings'));
 router.use('/products', require('./productsDuplicates'));
 router.use('/products', require('./productsBatch'));
+router.use('/products', require('./productsVariations'));
 router.use('/products', require('./products'));
 router.use('/products', require('./productsRanking'));
 router.use('/products', require('./productImage'));
